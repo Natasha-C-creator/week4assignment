709646cd25ba1d573c394decbfa9e55c18abd9eb
Natasha C <n.coward@hotmail.com>
2025-01-10 11:52:35 +0000

    Added required to the form fields

---
+++ server/seed.js
@@ -0,0 +1,6 @@
+// FROM SUPABASE:
+
+// INSERT INTO "visitor comments" ("FirstName", "Comments", "Rating")
+// VALUES
+// ('Natasha', 'Loved it', '5'),
+// ('John', 'Not sure', '2')
--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ import cors from "cors";
 
 const app = express();
 
-app.listen(3000, function () {
-  console.log("Server is listening on port 3000...");
+app.listen(5174, function () {
+  console.log("Server is listening on port 5174 ...");
 });
 
 dotenv.config();
@@ -18,4 +18,13 @@ const db = new pg.Pool({
 app.get("/visitorcomments", async function (request, response) {
   const comments = await db.query("SELECT * FROM comments WHERE id = 1");
   await response.json(comments.rows);
+});
+
+app.post("/new-data", async (req, res) => {
+  const data = req.body;
+  const query = await db.query(
+    `INSERT INTO visitorcomments (col,col2,col3) VALUES($1, $2, $3)`,
+    [data.input1, data.input2, data.input3]
+  );
+  await res.json(query.rows);
 });
